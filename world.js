c191d1bb26ce4ac4a44929f123e5b34c2f75fc6e
Abir Hasan <cse.170201063@gmail.com>
2022-09-03 02:17:01 +0600

    loading category data success

--- world.js
+++ world.js
@@ -11,11 +11,11 @@ const loadCatagories = () => {
 //  display Categories 
 const displayCategory = (categories) => {
     const categoriesContainer = document.getElementById('categories-container');
-   categories.forEach((category) => {
-       const div = document.createElement('div');
-       div.classList.add('hover:bg-red-600','hover:text-white')
+    categories.forEach((category) => {
+        const div = document.createElement('div');
+        div.classList.add('hover:bg-red-600', 'hover:text-white', 'rounded')
         div.innerHTML = `
-            <div class="text-center py-3" onclick="clickedCategory(${category.category_id})">
+            <div class="text-center py-3" onclick="clickedCategory('${category.category_id}')">
                 <p>${category.category_name}</p>
             </div> 
         `
@@ -27,10 +27,13 @@ loadCatagories()
 
 const clickedCategory = (categoryId) => {
     toggleSpinner(true);
-    loadCategoryData(categoryId);    
+    loadCategoryData(categoryId);
 }
 
-// ! Loading spinner
+
+// ! News section
+
+// Loading spinner
 const toggleSpinner = (isLoading) => {
     const spinner = document.getElementById('loader');
     if (isLoading) {
@@ -38,4 +41,18 @@ const toggleSpinner = (isLoading) => {
     } else {
         spinner.classList.add('hidden');
     }
+}
+
+// load category data
+const loadCategoryData = (id) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayCategoryData(data.data))
+        .catch(err => console.error(err));
+}
+
+// Display category data 
+const displayCategoryData = (newsFeed) => {
+    
 }
