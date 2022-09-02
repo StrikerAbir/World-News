74403ac9cd569abef9b0430aef81095abfb299de
Abir Hasan <cse.170201063@gmail.com>
2022-09-03 02:16:16 +0600

    loading spinner added category clicked working

--- world.js
+++ world.js
@@ -13,8 +13,9 @@ const displayCategory = (categories) => {
     const categoriesContainer = document.getElementById('categories-container');
    categories.forEach((category) => {
        const div = document.createElement('div');
+       div.classList.add('hover:bg-red-600','hover:text-white')
         div.innerHTML = `
-            <div id="single-category${category.category_id}" class="text-center py-3" onclick="clickedCategory(${category.category_id})">
+            <div class="text-center py-3" onclick="clickedCategory(${category.category_id})">
                 <p>${category.category_name}</p>
             </div> 
         `
@@ -22,3 +23,19 @@ const displayCategory = (categories) => {
     })
 }
 loadCatagories()
+
+
+const clickedCategory = (categoryId) => {
+    toggleSpinner(true);
+    loadCategoryData(categoryId);    
+}
+
+// ! Loading spinner
+const toggleSpinner = (isLoading) => {
+    const spinner = document.getElementById('loader');
+    if (isLoading) {
+        spinner.classList.remove('hidden');
+    } else {
+        spinner.classList.add('hidden');
+    }
+}
